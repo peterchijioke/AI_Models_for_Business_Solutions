e289775b7d3f5ec245a9d536d7c56543c99ab6f8
Chukwu (Chijioke)  Peter <peterchijioke1@gmail.com>
2025-03-22 18:12:14 +0100

    setup

--- app/_components/AIInsightsSection.tsx
+++ app/_components/AIInsightsSection.tsx
@@ -78,7 +78,7 @@ export default function AIInsightsSection() {
         ))}
       </div>
 
-      <Carousel className="w-full mt-6 max-w-[100%] p-0 m-0 sm:max-w-[100%]">
+      <Carousel className="w-full mt-6 max-w-[100%] bg-white p-0 m-0 sm:max-w-[100%]">
         <CarouselContent className="flex w-full">
           {TABS.map((item, index) => (
             <CarouselItem
